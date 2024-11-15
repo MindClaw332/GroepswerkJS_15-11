10446a030e7a040b7b62ab61e0475d92001bb6d6
Michael_Schrijvers_Thuis <michael.10330@hotmail.com>
2024-11-15 13:45:39 +0100

    added lightmode support

---
+++ core.js
@@ -0,0 +1,12 @@
+function toggleDarkMode(){
+    let darkTag = document.getElementById("theme")
+    const theme = darkTag.attributes[1];
+    console.log(darkTag.attributes[1]);
+    if(darkTag.attributes[1].nodeValue === "dark"){
+        theme.nodeValue = 'light'
+    } else {
+        theme.nodeValue = 'dark';
+        
+    }
+    
+}
