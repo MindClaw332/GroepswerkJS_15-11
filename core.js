a655757cd217d6e6d23fb1a6af3031ec36a07e54
Michael_Schrijvers_Thuis <michael.10330@hotmail.com>
2024-11-15 15:09:10 +0100

    added better looking layout following pico conventions

--- core.js
+++ core.js
@@ -1,10 +1,16 @@
 function toggleDarkMode(){
-    let darkTag = document.getElementById("theme")
+    let darkTag = document.getElementById("theme");
+    const lightbulb = document.getElementById("lightbulb");
     const theme = darkTag.attributes[1];
     console.log(darkTag.attributes[1]);
     if(darkTag.attributes[1].nodeValue === "dark"){
         theme.nodeValue = 'light'
+        // lightbulb.style.color = 'gray';
     } else {
         theme.nodeValue = 'dark';
+        // lightbulb.style.color = 'white'
     }
 }
+
+const click = document.getElementById('lightbulbtoggle');
+click.addEventListener('mousedown', toggleDarkMode)
