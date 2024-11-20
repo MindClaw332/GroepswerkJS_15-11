ecb142afc53f2f19bde777a63f7f199806e103af
Michael schrijvers <michael.10330@hotmail.com>
2024-11-20 15:22:14 +0100

    added remove and add functionality

--- core_refactor.js
+++ core_refactor.js
@@ -3,15 +3,17 @@ const inputNicelist = document.getElementById('nicelistinput');
 const inputLocation = document.getElementById('locationinput');
 const url = "http://localhost:3000/kids";
 const output = document.getElementById('kidlist');
-
+const addChildButton = document.getElementById('addchild')
 
 function DrawList() {
+    output.innerHTML = '';
     const localData = JSON.parse(localStorage.getItem('storedPosts'));
     localData.forEach(element => {
+        console.log(element, 'element print')
         output.innerHTML += `
             <div class="post-item" id="post-${element.kidid}">
                 <div class="grid post-content">
-                <h3>${element.kidname}, on naughtylist: ${element.nicelist} gifts: ${element.gifts}</h3>
+                <h3>${element.kidname}, naughtylist: ${element.nicelist}, location: ${element.location}, gifts: ${element.gifts}</h3>
                     <button type="button" onclick="editPost('${element.kidid}')">edit</button>
                 </div>            
                 <div class="edit-form" style="display: none;">
@@ -20,9 +22,9 @@ function DrawList() {
                     <input type="text" placeholder="location" id="locationedit">
                 </div>
                 <div class="grid">
-                    <button type="button" onclick="savetolocal('${element.kidid}', '${element.kidname}', '${element.nicelist}', '${element.gifts}',  '${element.location}')">save</button>
+                    <button type="button" onclick="savePost('${element.kidid}', '${element.kidname}', '${element.nicelist}', '${element.gifts}',  '${element.location}')">save</button>
                     <button type="button" onclick="cancelEdit(${element.kidid})">cancel</button>
-                    <button type="button" class="outline" id="deletebutton">delete</button>
+                    <button type="button" class="outline" onclick="deletePost(${element.kidid})">delete</button>
                     <button type="button">add book</button>
                     <button type="button">add toy</button>
                     <button type="button">add game</button>
@@ -50,13 +52,13 @@ function FetchDataAndSave() {
                 console.log('in de loop')
                 const newPost = {
                     kidid: element.id,
-                    kidname: element.name,
+                    kidname: element.kidname,
                     nicelist: element.nicelist,
                     gifts: element.gifts,
                     location: element.location
                 };
-                if(!savedPosts.some(p => p.kidid === newPost.kidid))
-                savedPosts.push(newPost);
+                if (!savedPosts.some(p => p.kidid === newPost.kidid))
+                    savedPosts.push(newPost);
             })
             localStorage.setItem('storedPosts', JSON.stringify(savedPosts));
             DrawList();
@@ -79,4 +81,39 @@ function cancelEdit(id) {
     postDiv.querySelector('.edit-form').style.display = 'none';
 }
 
+function AddPost() {
+    console.log('adding post')
+    const newPost = {
+        kidname: document.getElementById('nameinput').value || 'name unknown',
+        nicelist: document.getElementById('nicelistinput').value || 'true',
+        gifts: '[]',
+        location: document.getElementById('locationinput').value || 'unknown'
+    }
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPost)
+    })
+    .then(res => res.json())
+    .then(() => {
+        FetchDataAndSave();
+        document.getElementById('nameinput').value = '';
+        document.getElementById('nicelistinput').value = '';
+        document.getElementById('locationinput').value = '';
+    })
+
+}
+
+function deletePost(id) {
+    console.log('clicked delete')
+    fetch(`${url}/${id}`, {
+        method: 'DELETE'
+    })
+    .then(() => FetchDataAndSave())
+    .catch(e => console.error('Error deleting post:', e));
+}
+localStorage.clear();
+addChildButton.addEventListener('click', AddPost)
 FetchDataAndSave();
